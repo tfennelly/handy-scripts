7e05ed9fd519f2e1b8d3e384e7684846ba0d4908
Tom Fennelly <tom.fennelly@gmail.com>
2014-06-11 11:27:35 +0100

    Create get-plugin-repos.sh

---
+++ get-plugin-repos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Use curl -i https://api.github.com/orgs/jenkinsci/repos?page=XXX
+# to test for how many pages can be returned from the API.
+# After working that out, update the following variable.
+NUM_REPO_PAGES=42
+
+# delete the repo url list file
+rm repourls.txt
+
+# recreate the repo url list file
+for i in `seq 1 $NUM_REPO_PAGES`;
+do
+  echo "Getting page $i"
+  curl -i https://api.github.com/orgs/jenkinsci/repos?page=$i | grep clone_url  | sed 's/    "clone_url": "\(.*\)",/\1/g' >> repourls.txt
+done
+
+rm -rf repos
+
+# clone all the repos listed in the repo url list file
+mkdir repos
+pushd repos
+cat ../repourls.txt | while read giturl; do git clone "$giturl"; done
+popd
