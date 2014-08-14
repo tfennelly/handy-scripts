bf6636a332f9f08ab728000e2150425e4c8519de
Tom Fennelly <tom.fennelly@gmail.com>
2014-08-14 16:59:09 +0100

    Added -u $GITHUB_UN:$GITHUB_PW
    Was hitting unauthenticated API rate limits - need auth

--- get-plugin-repos.sh
+++ get-plugin-repos.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Before calling this script, you need to set your Github username
+# and password in the env e.g.:
+# export GITHUB_UN=<username>
+# export GITHUB_PW=<password>
+
 # Use curl -i https://api.github.com/orgs/jenkinsci/repos?page=XXX
 # to test for how many pages can be returned from the API.
 # After working that out, update the following variable.
@@ -12,7 +17,7 @@ rm repourls.txt
 for i in `seq 1 $NUM_REPO_PAGES`;
 do
   echo "Getting page $i"
-  curl -i https://api.github.com/orgs/jenkinsci/repos?page=$i | grep clone_url  | sed 's/    "clone_url": "\(.*\)",/\1/g' >> repourls.txt
+  curl -u $GITHUB_UN:$GITHUB_PW https://api.github.com/orgs/jenkinsci/repos?page=$i | grep clone_url  | sed 's/    "clone_url": "\(.*\)",/\1/g' >> repourls.txt
 done
 
 rm -rf repos
