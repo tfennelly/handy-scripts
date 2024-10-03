4c6859bc1b490f3952ff1199fa7ea74ca7bdaf3b
tfennelly <tom.fennelly@gmail.com>
2024-10-03 13:05:55 +0100

    source_script.sh

--- source_script.sh
+++ source_script.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "source_script.sh start"
-
 script_url=$1
 if [ -z "$script_url" ]; then
   echo "Usage: source_script.sh <URL>"
@@ -11,8 +9,6 @@ fi
 nowts=$(date +%s)
 temp_file=$(mktemp "script_temp.${nowts}.sh")
 
-echo "Downloading script from $script_url to $temp_file..."
 curl -s -o "$temp_file" "$script_url"
-echo "Sourcing script..."
 source "$temp_file"
 rm "$temp_file"
