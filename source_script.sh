aa19b4d35fa7bdd98d62198e1a061b18725f3c94
tfennelly <tom.fennelly@gmail.com>
2024-10-03 12:56:01 +0100

    source_script.sh

--- source_script.sh
+++ source_script.sh
@@ -2,17 +2,17 @@
 
 echo "source_script.sh start"
 
-#script_url=$1
-#if [ -z "$script_url" ]; then
-#  echo "Usage: source_script.sh <URL>"
-#  exit 1
-#fi
-#
-#nowts=$(date +%s)
-#temp_file=$(mktemp "script_temp.${nowts}.sh")
-#
-#echo "Downloading script from $script_url"
-#curl -s -o "$temp_file" "$script_url"
-#echo "Sourcing script..."
-#source "$temp_file"
-#rm "$temp_file"
+script_url=$1
+if [ -z "$script_url" ]; then
+  echo "Usage: source_script.sh <URL>"
+  exit 1
+fi
+
+nowts=$(date +%s)
+temp_file=$(mktemp "script_temp.${nowts}.sh")
+
+echo "Downloading script from $script_url"
+curl -s -o "$temp_file" "$script_url"
+echo "Sourcing script..."
+source "$temp_file"
+rm "$temp_file"
